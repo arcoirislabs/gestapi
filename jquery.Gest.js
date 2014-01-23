7fbafaf8252046f55f6d0a95d2cb191dcaf184b3
Amey Kshirsagar <alabs@alabs-HP-Pavilion-g6-Notebook-PC.(none)>
2014-01-23 09:40:57 +0530

    Able to write video inside a canvas tag for hand gesture analysis

--- jquery.Gest.js
+++ jquery.Gest.js
@@ -1,22 +1,38 @@
 (function($){
-	$.fn.Gest = function(){
-	function init() {
-		console.log('Ok');
-  		return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
+  $.fn.Gest = function(){
+  function init() {
+    console.log('Ok');
+      return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
             navigator.mozGetUserMedia || navigator.msGetUserMedia);
 }
-
+var localStream = null;
 if (init()) { 
-  $(this).html('<video id="feed" style="width:640px;height:480px;" autoplay>');
-  navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia ||
-            navigator.mozGetUserMedia || navigator.msGetUserMedia);
-  navigator.getUserMedia({video:true, audio:true,},function(stream){
-  	var video = document.getElementbyId('feed');
-  	var url = window.URL || window.webkitURL;  			
-  	video.src =url.createObjectURL(stream);
-  }, function(err){$(this).html('erroe');});
-} else {
-  alert('getUserMedia() is not supported in your browser');
-}
+  $(this).html('<video id="feed" style="width:'+window.innerWidth+'px; height:'+window.innerHeight+'px;-webkit-filter: grayscale(1);"  autoplay>');
+  $('<canvas></canvas>', {
+    id: 'pg',
+    style: 'display:block;width:'+window.innerWidth+'px; height:'+window.innerHeight+'px;',
+}).appendTo(this);
+  var c = document.querySelector('canvas');
+    var ctx = c.getContext('2d');
+  navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+  navigator.getUserMedia({video:true, audio:false,},
+    function(stream){
+    	var video = document.querySelector('video');
+   		var url = window.URL || window.webkitURL;       
+    	video.src =url.createObjectURL(stream);
+      localStream=stream;
+      if (localStream)
+      {
+      setInterval(function(){
+        console.log('drawing');
+        c.width = video.videoWidth;
+    c.height = video.videoHeight;
+        ctx.drawImage(video,0,0);
+      },20);
+    	}
+  }, 
+  //error function
+  function(err){console.log('erroe');});
+  } else {  alert('getUserMedia() is not supported in your browser');}
 }
 })(jQuery);
