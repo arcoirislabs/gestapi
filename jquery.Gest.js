0d52cdc35d63b96b68ae5ce3aef36e89c6b93388
Amey Kshirsagar <amey@arcoirislabs.com>
2014-03-17 19:56:57 +0530

    Successful Grid Detection
    Created grid based triggering. Detecting various areas of video feed as
    per the location of hand. Implemented Blend Mode Difference algorithm to
    detect the motion in video feed.

--- jquery.Gest.js
+++ jquery.Gest.js
@@ -1,88 +1,262 @@
-/*
-Gest API - a plugin that adds hand gesture control in your website
-
-Developer notes
-In this project I might be taking a long turn to implement hand gestures but this is the sequence I am following.
-The video source will be acting as eyes for this plugin. As we are using a hand (specifically a palm) to control the triggers. Basically the hand will be hand for us but for this plugin it will be a skin colored object inside a canvas frame. 
-Now we need to eliminate the background pixel data to uniquely identify the hand object in each video frame. 
-So we need to binarize the image by setting a threshold value that will represent the darkest tone of the skin colored object when place at the 
-max distance you want to operate (say till the touch pad of the laptop).
-By thresholding the image we are distinguishing the palm object colored in white and rest of the pixels in black.
-Then we are detecting the centroid of the palm object which will be returning us its location on canvas.
-This location will determine the nature of action and once it is detected it will trigger its respective callback function. 
-
-
-Usage:-
-
-$('#anytag').Gest('upper-left',function(){
-  mytrigger();
-});
-
-Types of actions supported:-
-*/
-
-
-
 (function($){
   $.fn.Gest = function(){
-  function init() {
-    console.log('Ok');
-      return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
-            navigator.mozGetUserMedia || navigator.msGetUserMedia);
-}
-var localStream = null;
-var lskinR =61;
-var lskinG =39;
-var lskinB = 52;
-var dskin = 3*38;
-if (init()) { 
-  $(this).html('<video id="feed" style="width:'+window.innerWidth+'px; height:'+window.innerHeight+'px;display:none;"  autoplay>');
-  $('<canvas></canvas>', {
-    id: 'pg',
-    style: 'display:block;width:'+window.innerWidth+'px; height:'+window.innerHeight+'px;',
-}).appendTo(this);
-  var c = document.querySelector('canvas');
-    var ctx = c.getContext('2d');
-    var threshold;
-  navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
-  navigator.getUserMedia({video:true, audio:false,},
-    function(stream){
-      var video = document.querySelector('video');
-      var url = window.URL || window.webkitURL;       
-      video.src =url.createObjectURL(stream);
-      localStream=stream;
-  if (localStream){
-    setInterval(function(){
-      //rendering video in canvas
-      c.width = video.videoWidth;
-      c.height = video.videoHeight;
-      ctx.drawImage(video,0,0);
-      
-      //grayscaling & binarising
-     var imgData = ctx.getImageData(0,0,c.width,c.height);
-        var pix  = imgData.data;
-        for (var i = 0, n = pix.length; i < n; i += 4){
-        var grayscale = pix[i] * .3 + pix[i+1] * .59 + pix[i+2] * .11 ;
-        pix[i  ] = grayscale;        // red
-        pix[i+1] = grayscale;        // green
-        pix[i+2] = grayscale;        // blue
-        //pix[i+3]              is alpha, but we are not concerned with it.
-    }
-    //redraw the image in black & white
-    //ctx.putImageData(imgData, 0, 0);
-    var GimgData = ctx.getImageData(0,0,c.width,c.height);
-    for (var i = 0, n = GimgData.data.length; i < n; i += 4){
-        
-    GimgData.data[i] = GimgData.data[i] == lskinR ? 255 : 0;
-    GimgData.data[i+1] = GimgData.data[i] == lskinG ? 255 : 0;
-    GimgData.data[i+2] = GimgData.data[i] == lskinB ? 255 : 0;
-    }
-    ctx.putImageData(GimgData, 0, 0);
-    },20);
-      }
-  }, 
-  //error function
-  function(err){console.log('erroe');});
-  } else {  alert('getUserMedia() is not supported in your browser');}
+	function hasGetUserMedia() {
+		// Note: Opera builds are unprefixed.
+		return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
+			navigator.mozGetUserMedia || navigator.msGetUserMedia);
+	}
+
+	if (hasGetUserMedia()) {
+		$("#info").hide();
+		$("#message").show();
+	} else {
+		$("#info").show();
+		$("#message").hide();
+		$("#video-demo").show();
+		$("#video-demo")[0].play();
+		return;
+	}
+
+	var webcamError = function(e) {
+		alert('Webcam error!', e);
+	};
+
+	var video = $('#webcam')[0];
+
+	if (navigator.getUserMedia) {
+		navigator.getUserMedia({audio: false, video: true}, function(stream) {
+			video.src = stream;
+			initialize();
+		}, webcamError);
+	} else if (navigator.webkitGetUserMedia) {
+		navigator.webkitGetUserMedia({audio: false, video: true}, function(stream) {
+			video.src = window.webkitURL.createObjectURL(stream);
+			main();
+		}, webcamError);
+	} else {
+		//video.src = 'somevideo.webm'; // fallback.
+	}
+	
+	var row1Pos1 = [0, 218, 430];
+	var row2Pos2 = [0, 218, 430];
+	var row3Pos3 = [0, 218, 430];
+
+	var timeOut, lastImageData;
+	var canvasSource = $("#canvas-source")[0];
+	var canvasBlended = $("#canvas-blended")[0];
+
+	var contextSource = canvasSource.getContext('2d');
+	var contextBlended = canvasBlended.getContext('2d');
+
+	var soundContext;
+	var bufferLoader;
+	var row1 = [];
+	var row2 = [];
+	var row3 = [];
+
+	// mirror video
+	contextSource.translate(canvasSource.width, 0);
+	contextSource.scale(-1, 1);
+
+	var c = 5;
+
+	function main() {
+		
+			load();
+		
+	}
+
+	function load() {
+			initRow1();
+			initRow2();
+			initRow3();
+		start();
+	}
+
+	function initRow1() {
+		for (var i=0; i<3; i++) {
+		
+			var note = {
+	
+				visual: $("#note" + i)[0]
+			};
+			note.area = {x:row1Pos1[i], y:0, width:note.visual.width, height:160};
+			row1.push(note);
+		}
+		
+	}
+	function initRow2() {
+		for (var i=3; i<6; i++) {
+		
+			var note1 = {
+	
+				visual: $("#note" + i)[0]
+			};
+			note1.area = {x:row2Pos2[i-3], y:160, width:note1.visual.width, height:160};
+			row2.push(note1);
+		}
+		console.log(row2);
+	}
+	function initRow3() {
+		for (var i=6; i<9; i++) {
+		
+			var note2 = {
+	
+				visual: $("#note" + i)[0]
+			};
+			note2.area = {x:row3Pos3[i-6], y:330, width:note2.visual.width, height:160};
+			row3.push(note2);
+		}
+		console.log(row3);
+	}
+
+	function start() {
+		$(canvasSource).show();
+		$(canvasBlended).show();
+		$("#grid").show();
+		$("#message").hide();
+		$("#description").show();
+		update();
+	}
+
+	function update() {
+		drawVideo();
+		blend();
+		checkAreas1();
+		checkAreas2();
+		checkAreas3();
+		timeOut = setTimeout(update, 20);
+	}
+
+	function drawVideo() {
+		contextSource.drawImage(video, 0, 0, video.width, video.height);
+	}
+
+	function blend() {
+		var width = canvasSource.width;
+		var height = canvasSource.height;
+		// get webcam image data
+		var sourceData = contextSource.getImageData(0, 0, width, height);
+		// create an image if the previous image doesn’t exist
+		if (!lastImageData) lastImageData = contextSource.getImageData(0, 0, width, height);
+		// create a ImageData instance to receive the blended result
+		var blendedData = contextSource.createImageData(width, height);
+		// blend the 2 images
+		differenceAccuracy(blendedData.data, sourceData.data, lastImageData.data);
+		// draw the result in a canvas
+		contextBlended.putImageData(blendedData, 0, 0);
+		// store the current webcam image
+		lastImageData = sourceData;
+	}
+
+	function fastAbs(value) {
+		// funky bitwise, equal Math.abs
+		return (value ^ (value >> 31)) - (value >> 31);
+	}
+
+	function threshold(value) {
+		return (value > 0x15) ? 0xFF : 0;
+	}
+
+	function difference(target, data1, data2) {
+		// blend mode difference
+		if (data1.length != data2.length) return null;
+		var i = 0;
+		while (i < (data1.length * 0.25)) {
+			target[4*i] = data1[4*i] == 0 ? 0 : fastAbs(data1[4*i] - data2[4*i]);
+			target[4*i+1] = data1[4*i+1] == 0 ? 0 : fastAbs(data1[4*i+1] - data2[4*i+1]);
+			target[4*i+2] = data1[4*i+2] == 0 ? 0 : fastAbs(data1[4*i+2] - data2[4*i+2]);
+			target[4*i+3] = 0xFF;
+			++i;
+		}
+	}
+
+	function differenceAccuracy(target, data1, data2) {
+		if (data1.length != data2.length) return null;
+		var i = 0;
+		while (i < (data1.length * 0.25)) {
+			var average1 = (data1[4*i] + data1[4*i+1] + data1[4*i+2]) / 3;
+			var average2 = (data2[4*i] + data2[4*i+1] + data2[4*i+2]) / 3;
+			var diff = threshold(fastAbs(average1 - average2));
+			target[4*i] = diff;
+			target[4*i+1] = diff;
+			target[4*i+2] = diff;
+			target[4*i+3] = 0xFF;
+			++i;
+		}
+	}
+
+	function checkAreas1() {
+		// loop over the note areas
+		for (var r=0; r<3; ++r) {
+			// get the pixels in a note area from the blended image
+			var blendedData = contextBlended.getImageData(row1[r].area.x, row1[r].area.y, row1[r].area.width, row1[r].area.height);
+			var i = 0;
+			var average = 0;
+			// loop over the pixels
+			while (i < (blendedData.data.length * 0.25)) {
+				// make an average between the color channel
+				average += (blendedData.data[i*4] + blendedData.data[i*4+1] + blendedData.data[i*4+2]) / 3;
+				++i;
+			}
+			// calculate an average between of the color values of the note area
+			average = Math.round(average / (blendedData.data.length * 0.25));
+			if (average > 10) {
+				// over a small limit, consider that a movement is detected
+				// play a note and show a visual feedback to the user
+				console.log(r);
+				row1[r].visual.style.display = "block";
+				$(row1[r].visual).fadeOut();
+			}
+		}
+	}
+	function checkAreas2() {
+		// loop over the note areas
+		for (var r=3; r<6; ++r) {
+			// get the pixels in a note area from the blended image
+			var blendedData = contextBlended.getImageData(row2[r-3].area.x, row2[r-3].area.y, row2[r-3].area.width, row2[r-3].area.height);
+			var i = 0;
+			var average = 0;
+			// loop over the pixels
+			while (i < (blendedData.data.length * 0.25)) {
+				// make an average between the color channel
+				average += (blendedData.data[i*4] + blendedData.data[i*4+1] + blendedData.data[i*4+2]) / 3;
+				++i;
+			}
+			// calculate an average between of the color values of the note area
+			average = Math.round(average / (blendedData.data.length * 0.25));
+			if (average > 10) {
+				// over a small limit, consider that a movement is detected
+				// play a note and show a visual feedback to the user
+				console.log(r);
+				row2[r-3].visual.style.display = "block";
+				$(row2[r-3].visual).fadeOut();
+			}
+		}
+	}
+	function checkAreas3() {
+		// loop over the note areas
+		for (var r=6; r<9; ++r) {
+			// get the pixels in a note area from the blended image
+			var blendedData = contextBlended.getImageData(row3[r-6].area.x, row3[r-6].area.y, row3[r-6].area.width, row3[r-6].area.height);
+			var i = 0;
+			var average = 0;
+			// loop over the pixels
+			while (i < (blendedData.data.length * 0.25)) {
+				// make an average between the color channel
+				average += (blendedData.data[i*4] + blendedData.data[i*4+1] + blendedData.data[i*4+2]) / 3;
+				++i;
+			}
+			// calculate an average between of the color values of the note area
+			average = Math.round(average / (blendedData.data.length * 0.25));
+			if (average > 5) {
+				// over a small limit, consider that a movement is detected
+				// play a note and show a visual feedback to the user
+				console.log(r);
+				row3[r-6].visual.style.display = "block";
+				$(row3[r-6].visual).fadeOut();
+			}
+		}
+	}	
 }
 })(jQuery);
---
+++ js/app.js
@@ -0,0 +1,262 @@
+(function() {
+
+	function hasGetUserMedia() {
+		// Note: Opera builds are unprefixed.
+		return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
+			navigator.mozGetUserMedia || navigator.msGetUserMedia);
+	}
+
+	if (hasGetUserMedia()) {
+		$("#info").hide();
+		$("#message").show();
+	} else {
+		$("#info").show();
+		$("#message").hide();
+		$("#video-demo").show();
+		$("#video-demo")[0].play();
+		return;
+	}
+
+	var webcamError = function(e) {
+		alert('Webcam error!', e);
+	};
+
+	var video = $('#webcam')[0];
+
+	if (navigator.getUserMedia) {
+		navigator.getUserMedia({audio: false, video: true}, function(stream) {
+			video.src = stream;
+			initialize();
+		}, webcamError);
+	} else if (navigator.webkitGetUserMedia) {
+		navigator.webkitGetUserMedia({audio: false, video: true}, function(stream) {
+			video.src = window.webkitURL.createObjectURL(stream);
+		}, webcamError);
+	} else {
+		//video.src = 'somevideo.webm'; // fallback.
+	}
+	 $.fn.Gest = function(){
+	 	main();
+	}
+	var row1Pos1 = [0, 218, 430];
+	var row2Pos2 = [0, 218, 430];
+	var row3Pos3 = [0, 218, 430];
+
+	var timeOut, lastImageData;
+	var canvasSource = $("#canvas-source")[0];
+	var canvasBlended = $("#canvas-blended")[0];
+
+	var contextSource = canvasSource.getContext('2d');
+	var contextBlended = canvasBlended.getContext('2d');
+
+	var soundContext;
+	var bufferLoader;
+	var row1 = [];
+	var row2 = [];
+	var row3 = [];
+
+	// mirror video
+	contextSource.translate(canvasSource.width, 0);
+	contextSource.scale(-1, 1);
+
+	var c = 5;
+
+	function main() {
+		
+			load();
+		
+	}
+
+	function load() {
+			initRow1();
+			initRow2();
+			initRow3();
+		start();
+	}
+
+	function initRow1() {
+		for (var i=0; i<3; i++) {
+		
+			var note = {
+	
+				visual: $("#note" + i)[0]
+			};
+			note.area = {x:row1Pos1[i], y:0, width:note.visual.width, height:160};
+			row1.push(note);
+		}
+		
+	}
+	function initRow2() {
+		for (var i=3; i<6; i++) {
+		
+			var note1 = {
+	
+				visual: $("#note" + i)[0]
+			};
+			note1.area = {x:row2Pos2[i-3], y:160, width:note1.visual.width, height:160};
+			row2.push(note1);
+		}
+		console.log(row2);
+	}
+	function initRow3() {
+		for (var i=6; i<9; i++) {
+		
+			var note2 = {
+	
+				visual: $("#note" + i)[0]
+			};
+			note2.area = {x:row3Pos3[i-6], y:330, width:note2.visual.width, height:160};
+			row3.push(note2);
+		}
+		console.log(row3);
+	}
+
+	function start() {
+		$(canvasSource).show();
+		$(canvasBlended).show();
+		$("#grid").show();
+		$("#message").hide();
+		$("#description").show();
+		update();
+	}
+
+	function update() {
+		drawVideo();
+		blend();
+		checkAreas1();
+		checkAreas2();
+		checkAreas3();
+		timeOut = setTimeout(update, 20);
+	}
+
+	function drawVideo() {
+		contextSource.drawImage(video, 0, 0, video.width, video.height);
+	}
+
+	function blend() {
+		var width = canvasSource.width;
+		var height = canvasSource.height;
+		// get webcam image data
+		var sourceData = contextSource.getImageData(0, 0, width, height);
+		// create an image if the previous image doesn’t exist
+		if (!lastImageData) lastImageData = contextSource.getImageData(0, 0, width, height);
+		// create a ImageData instance to receive the blended result
+		var blendedData = contextSource.createImageData(width, height);
+		// blend the 2 images
+		differenceAccuracy(blendedData.data, sourceData.data, lastImageData.data);
+		// draw the result in a canvas
+		contextBlended.putImageData(blendedData, 0, 0);
+		// store the current webcam image
+		lastImageData = sourceData;
+	}
+
+	function fastAbs(value) {
+		// funky bitwise, equal Math.abs
+		return (value ^ (value >> 31)) - (value >> 31);
+	}
+
+	function threshold(value) {
+		return (value > 0x15) ? 0xFF : 0;
+	}
+
+	function difference(target, data1, data2) {
+		// blend mode difference
+		if (data1.length != data2.length) return null;
+		var i = 0;
+		while (i < (data1.length * 0.25)) {
+			target[4*i] = data1[4*i] == 0 ? 0 : fastAbs(data1[4*i] - data2[4*i]);
+			target[4*i+1] = data1[4*i+1] == 0 ? 0 : fastAbs(data1[4*i+1] - data2[4*i+1]);
+			target[4*i+2] = data1[4*i+2] == 0 ? 0 : fastAbs(data1[4*i+2] - data2[4*i+2]);
+			target[4*i+3] = 0xFF;
+			++i;
+		}
+	}
+
+	function differenceAccuracy(target, data1, data2) {
+		if (data1.length != data2.length) return null;
+		var i = 0;
+		while (i < (data1.length * 0.25)) {
+			var average1 = (data1[4*i] + data1[4*i+1] + data1[4*i+2]) / 3;
+			var average2 = (data2[4*i] + data2[4*i+1] + data2[4*i+2]) / 3;
+			var diff = threshold(fastAbs(average1 - average2));
+			target[4*i] = diff;
+			target[4*i+1] = diff;
+			target[4*i+2] = diff;
+			target[4*i+3] = 0xFF;
+			++i;
+		}
+	}
+
+	function checkAreas1() {
+		// loop over the note areas
+		for (var r=0; r<3; ++r) {
+			// get the pixels in a note area from the blended image
+			var blendedData = contextBlended.getImageData(row1[r].area.x, row1[r].area.y, row1[r].area.width, row1[r].area.height);
+			var i = 0;
+			var average = 0;
+			// loop over the pixels
+			while (i < (blendedData.data.length * 0.25)) {
+				// make an average between the color channel
+				average += (blendedData.data[i*4] + blendedData.data[i*4+1] + blendedData.data[i*4+2]) / 3;
+				++i;
+			}
+			// calculate an average between of the color values of the note area
+			average = Math.round(average / (blendedData.data.length * 0.25));
+			if (average > 10) {
+				// over a small limit, consider that a movement is detected
+				// play a note and show a visual feedback to the user
+				console.log(r);
+				row1[r].visual.style.display = "block";
+				$(row1[r].visual).fadeOut();
+			}
+		}
+	}
+	function checkAreas2() {
+		// loop over the note areas
+		for (var r=3; r<6; ++r) {
+			// get the pixels in a note area from the blended image
+			var blendedData = contextBlended.getImageData(row2[r-3].area.x, row2[r-3].area.y, row2[r-3].area.width, row2[r-3].area.height);
+			var i = 0;
+			var average = 0;
+			// loop over the pixels
+			while (i < (blendedData.data.length * 0.25)) {
+				// make an average between the color channel
+				average += (blendedData.data[i*4] + blendedData.data[i*4+1] + blendedData.data[i*4+2]) / 3;
+				++i;
+			}
+			// calculate an average between of the color values of the note area
+			average = Math.round(average / (blendedData.data.length * 0.25));
+			if (average > 10) {
+				// over a small limit, consider that a movement is detected
+				// play a note and show a visual feedback to the user
+				console.log(r);
+				row2[r-3].visual.style.display = "block";
+				$(row2[r-3].visual).fadeOut();
+			}
+		}
+	}
+	function checkAreas3() {
+		// loop over the note areas
+		for (var r=6; r<9; ++r) {
+			// get the pixels in a note area from the blended image
+			var blendedData = contextBlended.getImageData(row3[r-6].area.x, row3[r-6].area.y, row3[r-6].area.width, row3[r-6].area.height);
+			var i = 0;
+			var average = 0;
+			// loop over the pixels
+			while (i < (blendedData.data.length * 0.25)) {
+				// make an average between the color channel
+				average += (blendedData.data[i*4] + blendedData.data[i*4+1] + blendedData.data[i*4+2]) / 3;
+				++i;
+			}
+			// calculate an average between of the color values of the note area
+			average = Math.round(average / (blendedData.data.length * 0.25));
+			if (average > 5) {
+				// over a small limit, consider that a movement is detected
+				// play a note and show a visual feedback to the user
+				console.log(r);
+				row3[r-6].visual.style.display = "block";
+				$(row3[r-6].visual).fadeOut();
+			}
+		}
+	}
+})();
